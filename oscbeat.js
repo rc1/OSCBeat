aba6cf73764887ce11aedbc4c9026a489dca0c82
Ross Cairns <ross@electricglen.com>
2012-06-12 16:35:38 +0100

    cleaned the command line and added an address option

--- oscbeat.js
+++ oscbeat.js
@@ -3,19 +3,22 @@ var  program = require('commander');
 program.version('0.0.1')
     .option('-p, --port [number]', 'Port to send messages to', 3333)
     .option('-h, --host [address]', 'IP addresst to send messages to', '127.0.0.1')
-    .option('-b, --bpm [number]', 'Beats per minute')
+    .option('-b, --bpm [number]', 'Beats per minute', 128)
+    .option('-a, --address [address]', 'OSC Address', '/beat/')
     .parse(process.argv);
 
-console.log(program.port);
-
-var bpm = (program.bpm)?60000/program.bpm:10000;
-
+var bpm = 60000/program.bpm;
 var osc = require('node-osc');
 var client = new osc.Client(program.host,  program.port);
 var beatcount = 0;
 setInterval(function () {
-    client.send("/beat/", ++beatcount);
-    if (beatcount === 4) { beatcount = 0; }
+    client.send(program.address, ++beatcount);
+    if (beatcount === 4) { 
+        process.stdout.write("|");
+        beatcount = 0; 
+    } else {
+        process.stdout.write("+");
+    }
 }, bpm);
 
-console.log("Sending",bpm,"to",program.host,"on port", program.port);
+console.log("At", program.bpm,"sending the address '", program.address, "' to ip", program.host,"on port", program.port);
